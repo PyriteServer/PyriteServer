23c6a7dbee508f07c4bc4f4ad0ef20a79f8e3753
stephbu <stephbu@microsoft.com>
2015-03-23 00:08:12 -0700

    Route value constraints

--- CubeServer/Controllers/CubeController.cs
+++ CubeServer/Controllers/CubeController.cs
@@ -12,7 +12,7 @@ namespace CubeServer.Controllers
     public class CubeController : ApiController
     {
         [HttpGet]
-        [Route("sets/{setid}/{version}/cubes/{detail}/{x},{y},{z},{h},{w},{d}")]
+        [Route("sets/{setid}/{version}/cubes/{detail}/{x:float},{y:float},{z:float},{h:float},{w:float},{d:float}")]
         public IEnumerable<object> Get(string setid, string version, string detail, float x, float y, float z, float h, float w, float d)
         {
             return new string[] { "cube1", "cube2" };
