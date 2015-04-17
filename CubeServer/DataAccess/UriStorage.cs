6cacc9bcdb1521e2bb710b6bd6ffb465c5f0dd5d
stephbu <stephbu@microsoft.com>
2015-04-17 02:49:42 -0700

    Refactoring and Cube Query Testing

--- CubeServer/DataAccess/UriStorage.cs
+++ CubeServer/DataAccess/UriStorage.cs
@@ -269,95 +269,20 @@ namespace CubeServer.DataAccess
 
         public IEnumerable<int[]> Query(string setId, string versionId, string detail, BoundingBox worldBox)
         {
-            LoaderResults setData = this.loadedSetData.Get();
-            if (setData == null)
-            {
-                throw new NotFoundException("setData");
-            }
-
-            SetVersion setVersion = setData.FindSetVersion(setId, versionId);
-
-            SetVersionLevelOfDetail lod = setVersion.DetailLevels.FirstOrDefault(l => l.Name == detail);
-            if (lod == null)
-            {
-                throw new NotFoundException("detailLevel");
-            }
-
-            BoundingBox cubeBox = lod.ToCubeCoordinates(worldBox);
-            IEnumerable<Intersection<CubeBounds>> intersections = lod.Cubes.AllIntersections(cubeBox);
-
-            foreach (Intersection<CubeBounds> intersection in intersections)
-            {
-                Vector3 min = intersection.Object.BoundingBox.Min;
-                yield return new[] { (int)min.X, (int)min.Y, (int)min.Z };
-            }
+            SetVersion setVersion = this.loadedSetData.Get().FindSetVersion(setId, versionId);
+            return setVersion.Query(detail, worldBox);
         }
 
         public IEnumerable<QueryDetailContract> Query(string setId, string versionId, string profile, BoundingSphere worldSphere)
         {
-            ProfileLevel[] profiles = ParseProfile(profile).ToArray();
             SetVersion setVersion = this.loadedSetData.Get().FindSetVersion(setId, versionId);
-
-            // TODO: Move this dictionary into SetVersion
-            Dictionary<string, SetVersionLevelOfDetail> detailLevels = setVersion.DetailLevels.ToDictionary(
-                lod => lod.Name,
-                lod => lod,
-                StringComparer.OrdinalIgnoreCase);
-            int sumProportions = profiles.Sum(p => p.Proportion);
-
-            float radiusProportionRatio = worldSphere.Radius / sumProportions;
-
-            int runningTotal = 0;
-
-            var queries =
-                profiles.Select(p => new { p.Level, Radius = runningTotal += p.Proportion })
-                    .Select(p => new { p.Level, Radius = p.Radius * radiusProportionRatio });
-
-            foreach (var query in queries)
-            {
-                SetVersionLevelOfDetail detailLevel;
-
-                if (!detailLevels.TryGetValue(query.Level, out detailLevel))
-                {
-                    throw new NotFoundException("detail level");
-                }
-
-                Vector3 cubeCenter = detailLevel.ToCubeCoordinates(worldSphere.Center);
-
-                // TODO: Spheres in World Space aren't spheres in cube space, so this factor distorts the query if 
-                // there is variation in scaling factor for different dimensions e.g. 3,2,1
-                float cubeRadius = detailLevel.ToCubeCoordinates(new Vector3(worldSphere.Radius, 0, 0)).X;
-
-                IEnumerable<Intersection<CubeBounds>> queryResults = detailLevel.Cubes.AllIntersections(new BoundingSphere(cubeCenter, cubeRadius));
-
-                yield return
-                    new QueryDetailContract
-                    {
-                        Name = detailLevel.Name,
-                        Cubes = queryResults.Select(i => i.Object.BoundingBox.Min).Select(v => new[] { (int)v.X, (int)v.Y, (int)v.Z })
-                    };
-            }
+            return setVersion.Query(profile, worldSphere);
         }
 
         public IEnumerable<QueryDetailContract> Query(string setId, string versionId, Vector3 worldCenter)
         {
             SetVersion setVersion = this.loadedSetData.Get().FindSetVersion(setId, versionId);
-
-            foreach (var lod in setVersion.DetailLevels)
-            {
-                Vector3 cubeCenter = lod.ToCubeCoordinates(worldCenter);
-                Vector3 flooredCube = new Vector3((int)cubeCenter.X, (int)cubeCenter.Y,(int)cubeCenter.Z);
-                BoundingBox rubiksCube = new BoundingBox(flooredCube - Vector3.One, flooredCube + Vector3.One);
-
-                IEnumerable<Intersection<CubeBounds>> queryResults = lod.Cubes.AllIntersections(rubiksCube);
-
-                yield return
-                    new QueryDetailContract
-                    {
-                        Name = lod.Name,
-                        Cubes = queryResults.Select(i => i.Object.BoundingBox.Min).Select(v => new[] { (int)v.X, (int)v.Y, (int)v.Z })
-                    };
-            }
+            return setVersion.Query(worldCenter);
         }
 
         protected virtual void Dispose(bool disposing)
@@ -414,26 +339,6 @@ namespace CubeServer.DataAccess
             modelPath = modelPath.Replace(Z_PLACEHOLDER, zpos.ToString());
             modelPath = modelPath.Replace(FORMAT_PLACEHOLDER, format.ToString().ToLower());
             return modelPath;
-        }
-
-        private static IEnumerable<ProfileLevel> ParseProfile(string profileString)
-        {
-            foreach (string level in profileString.Split(ProfileLevel.LevelDelimiter, StringSplitOptions.RemoveEmptyEntries))
-            {
-                string[] levelSplit = level.Split(ProfileLevel.ProportionDelimiter);
-                if (levelSplit.Length != 2)
-                {
-                    continue;
-                }
-
-                int proportion;
-                if (!int.TryParse(levelSplit[1], out proportion))
-                {
-                    continue;
-                }
-
-                yield return new ProfileLevel { Level = levelSplit[0], Proportion = proportion };
-            }
         }
 
         private T DeserializeStream<T>(Stream stream)
@@ -543,14 +448,6 @@ namespace CubeServer.DataAccess
                     Thread.Sleep(pollingPeriod);
                 }
             }
-        }
-
-        private struct ProfileLevel
-        {
-            internal static char[] LevelDelimiter = new char[] { ',' };
-            internal static char[] ProportionDelimiter = new char[] { '=' };
-            internal string Level;
-            internal int Proportion;
         }
 
         private class RevolvingState<T> : IDisposable
---
+++ CubeServerTest/QueryTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CubeServerTest
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using CubeServer;
+    using CubeServer.Contracts;
+    using CubeServer.DataAccess.Json;
+    using CubeServer.Model;
+    using Microsoft.Xna.Framework;
+
+    [TestClass]
+    public class QueryTest
+    {
+        private readonly BoundingBox oneBoundingBox = new BoundingBox(Vector3.Zero, Vector3.One);
+        private readonly BoundingBox zeroBoundingBox = new BoundingBox(Vector3.Zero, Vector3.Zero);
+
+        [TestMethod]
+        public void TestRubik1()
+        {
+            SetVersion setVersion = this.Get4x4x4();
+            var results = setVersion.Query(new Vector3(15,15,15)).ToArray();
+            Assert.AreEqual(1, results.Length);
+            Assert.AreEqual(3, results[0].Cubes.Count());
+            Assert.AreEqual("0,0,0", results[0].Cubes.Select(i => String.Format("{0},{1},{2}", i[0], i[1], i[2])).First()); 
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void TestRubik2()
+        {
+            SetVersion setVersion = this.Get4x4x4();
+            var results = setVersion.Query(new Vector3(25, 25, 25)).ToArray();
+            Assert.AreEqual(1, results.Length);
+            Assert.AreEqual(3, results[0].Cubes.Count());
+            Assert.AreEqual("1,1,1", results[0].Cubes.Select(i => String.Format("{0},{1},{2}", i[0], i[1], i[2])).First());
+        }
+
+        public SetVersion Get4x4x4()
+        {
+            SetVersion setVersion = new SetVersion();
+            setVersion.Name = "Test";
+            setVersion.Version = "v1";
+
+            List<CubeBounds> testBounds = new List<CubeBounds>();
+
+            testBounds.Add(new CubeBounds { BoundingBox = this.MakeCube(Vector3.Zero, 1) });
+            testBounds.Add(new CubeBounds { BoundingBox = this.MakeCube(new Vector3(1, 1, 1), 1) });
+            testBounds.Add(new CubeBounds { BoundingBox = this.MakeCube(new Vector3(2, 2, 2), 1) });
+            testBounds.Add(new CubeBounds { BoundingBox = this.MakeCube(new Vector3(3, 3, 3), 1) });
+
+            var ocTree = new OcTree<CubeBounds>(this.zeroBoundingBox, testBounds);
+            ocTree.UpdateTree();
+
+            SetVersionLevelOfDetail lod = new SetVersionLevelOfDetail
+                                          {
+                                              Name = "L1",
+                                              Cubes = ocTree,
+                                              SetSize = ocTree.Region.Max - ocTree.Region.Min,
+                                              VirtualWorldBounds = new BoundingBox(Vector3.Zero, new Vector3(40,40,40))
+                                          };
+
+            setVersion.DetailLevels = new []{ lod };
+            return setVersion;
+        }
+   
+        private BoundingBox MakeCube(Vector3 min, float size)
+        {
+            Vector3 max = new Vector3 { X = min.X + size, Y = min.Y + size, Z = min.Z + size };
+            return new BoundingBox { Min = min, Max = max };
+        }
+    }
+
+}
