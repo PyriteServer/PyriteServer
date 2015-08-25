0a1b9abc13b4f5ab20d19993dbdcc7f993d5008c
stephbu <stephbu@microsoft.com>
2015-08-25 11:36:55 -0700

    AppSettings Secrets Provider

---
+++ PyriteServer/DataAccess/AzureAppSettingsProvider.cs
@@ -0,0 +1,21 @@
+﻿namespace PyriteServer.DataAccess
+{
+    using System.Configuration;
+    using PyriteServer.Contracts;
+
+    /// <summary>Reads account secrets information from Azure AppSettings at runtime</summary>
+    public class AzureAppSettingsProvider : ISecretsProvider
+    {
+        public AzureAppSettingsProvider()
+        {
+        }
+
+        public string Value
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["AccountKey"];
+            }
+        }
+    }
+}
